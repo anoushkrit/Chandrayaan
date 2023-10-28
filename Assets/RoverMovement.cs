bb8ab6ebff50f6bcea70f53ff6c940cc2ccc9a89
Anoushkrit Goel <anoushkritgoel@gmail.com>
2023-10-28 14:47:51 +0530

    mouse camera movement and scroll zoom in and zoom out feature added

--- Assets/RoverMovement.cs
+++ Assets/RoverMovement.cs
@@ -11,28 +11,41 @@ public class RoverMovement : MonoBehaviour
     private Rigidbody rb;
 
     // Camera component of the scene
-    private Camera mainCamera;
+    private FollowCamera followCamera;
+
+    // Rotation speed of the camera
+    public float cameraRotationSpeed = 5.0f;
+
+    // Minimum and maximum pitch angle of the camera
+    public float minPitchAngle = -60.0f;
+    public float maxPitchAngle = 60.0f;
+
+    // Current pitch angle of the camera
+    private float currentPitchAngle = 0.0f;
+
+    // Zoom speed of the camera
+    public float zoomSpeed = 5.0f;
+
+    // Minimum and maximum distance of the camera from the rover
+    public float minDistance = 5.0f;
+    public float maxDistance = 20.0f;
+
+    // Current distance of the camera from the rover
+    private float currentDistance = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Find the object by its name
-        GameObject roverObject = GameObject.Find("Rover");
-
-        // Get the RoverMovement component of the rover object
-        RoverMovement roverMovement = roverObject.GetComponent<RoverMovement>();
-
         // Get the Rigidbody component of the rover
         rb = GetComponent<Rigidbody>();
 
-        // Find all the cameras in the scene
-        Camera[] cameras = FindObjectsOfType<Camera>();
+        // Find the FollowCamera object in the scene
+        followCamera = GameObject.Find("FollowCamera").GetComponent<FollowCamera>();
 
-        // Assign the camera object to the first camera found
-        if (cameras.Length > 0)
-        {
-            mainCamera = cameras[0];
-        }
+
+        // Set the followCamera as the active camera
+        followCamera.gameObject.SetActive(true);
+        followCamera.transform.position = new Vector3(173.85f, 77.5f, 133.85f);
 
         // Raycast to detect the ground below the rover
         RaycastHit hit;
@@ -47,33 +60,75 @@ public class RoverMovement : MonoBehaviour
     void FixedUpdate()
     {
         // Check if the Rigidbody component is not null
-        // Move the rover forward when the up arrow key is pressed
-        if (Input.GetKey(KeyCode.UpArrow))
+        // Move the rover forward when the W key is pressed
+        if (Input.GetKey(KeyCode.W))
         {
             rb.AddForce(transform.forward * speed);
         }
 
-        // Move the rover backward when the down arrow key is pressed
-        if (Input.GetKey(KeyCode.DownArrow))
+        // Move the rover backward when the S key is pressed
+        if (Input.GetKey(KeyCode.S))
         {
             rb.AddForce(-transform.forward * speed);
         }
 
-        // Rotate the rover left when the left arrow key is pressed
-        if (Input.GetKey(KeyCode.LeftArrow))
+        // Rotate the rover left when the A key is pressed
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.down * speed * Time.deltaTime);
         }
 
-        // Rotate the rover right when the right arrow key is pressed
-        if (Input.GetKey(KeyCode.RightArrow))
+        // Rotate the rover right when the D key is pressed
+        if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.up * speed * Time.deltaTime);
         }
 
         // Move the camera to follow the rover's movement from a 3rd person view
-        mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y + 10.0f, transform.position.z - 10.0f);
-        mainCamera.transform.rotation = Quaternion.Euler(45.0f, 0.0f, 0.0f);
+        if (followCamera != null)
+        {
+            // Move the camera up when the Up arrow key is pressed
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                followCamera.transform.position += Vector3.up * speed * Time.deltaTime;
+            }
+
+            // Move the camera down when the Down arrow key is pressed
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                followCamera.transform.position += Vector3.down * speed * Time.deltaTime;
+            }
+
+            // Move the camera right when the Right arrow key is pressed
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                followCamera.transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+
+            // Move the camera left when the Left arrow key is pressed
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                followCamera.transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+
+            // Zoom in and out with the mouse wheel
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            currentDistance -= scroll * zoomSpeed;
+            currentDistance = Mathf.Clamp(currentDistance, minDistance, maxDistance);
+
+            // Rotate the camera with the mouse
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            currentPitchAngle -= mouseY * cameraRotationSpeed;
+            currentPitchAngle = Mathf.Clamp(currentPitchAngle, minPitchAngle, maxPitchAngle);
+
+            followCamera.transform.RotateAround(transform.position, Vector3.up, mouseX * cameraRotationSpeed);
+            followCamera.transform.localEulerAngles = new Vector3(currentPitchAngle, followCamera.transform.localEulerAngles.y, followCamera.transform.localEulerAngles.z);
+
+            followCamera.transform.position = transform.position - followCamera.transform.forward * currentDistance;
+            followCamera.transform.LookAt(transform.position);
+        }
     }
 
     // Set the drag and angularDrag properties of the Rigidbody component to reduce the force taking the gravity into consideration
